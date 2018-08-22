08db6a4d607e0638da3d63b1a36c8c6184144e49
Dima Koltovich <steklopod@gmail.com>
2018-08-22 15:37:47 +0300

    Test for first parsing function, redme refactor

--- src/main/scala/ru/steklopod/SimpleParser.scala
+++ src/main/scala/ru/steklopod/SimpleParser.scala
@@ -1,12 +1,12 @@
 package ru.steklopod
 
-import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.combinator._
 
 class SimpleParser extends RegexParsers {
   def word: Parser[String] = """[a-z]+""".r ^^ { _.toString }
 
 }
 
-object SimpleParserTest extends App {
-
+object SimpleParserTest extends SimpleParser with App {
+  println(parse(word, "johnny come lately"))
 }
