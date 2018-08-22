0c7f675bdcd131ad5eec21824bba9d0f675e3a47
Dima Koltovich <steklopod@gmail.com>
2018-08-22 17:07:48 +0300

    readme finished

--- src/main/scala/ru/steklopod/SimpleParser.scala
+++ src/main/scala/ru/steklopod/SimpleParser.scala
@@ -3,11 +3,11 @@ package ru.steklopod
 import scala.util.parsing.combinator._
 
 class SimpleParser extends RegexParsers {
-  def word: Parser[String] =
-    """[а-я]+""".r ^^ {
-      _.toString
-    }
+  def word:   Parser[String]   = """[а-я]+""".r       ^^ { _.toString }
+  def number: Parser[Int]      = """(0|[1-9]\d*)""".r ^^ { _.toInt }
+  def freq:   Parser[WordFreq] = word ~ number        ^^ { case wd ~ fr => WordFreq(wd,fr) }
 }
+
 
 object SimpleParserTest extends SimpleParser with App {
   parse(word, "дима придет поздно") match {
@@ -22,3 +22,11 @@ case class WordFreq(word: String, count: Int) {
 }
 
 
+object TestSimpleParser extends SimpleParser with App{
+    parse(freq, "дима 121") match {
+      case Success(matched,_) => println(matched)
+      case Failure(msg,_) => println("FAILURE: " + msg)
+      case Error(msg,_) => println("ERROR: " + msg)
+    }
+
+}
